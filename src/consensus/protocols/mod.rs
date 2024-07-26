e84792d9abb7a7a930da522e52ef6d230fcec401
Shubham Mishra <grapheo12@gmail.com>
2024-07-26 10:30:46 +0000

    Quorum Diversity starter code

--- src/consensus/protocols/mod.rs
+++ src/consensus/protocols/mod.rs
@@ -8,12 +8,17 @@ mod signed_raft;
 #[cfg(feature = "signed_raft")]
 pub use signed_raft::*;
 
+#[cfg(feature = "diverse_raft")]
+mod diverse_raft;
+#[cfg(feature = "diverse_raft")]
+pub use diverse_raft::*;
+
 #[cfg(feature = "jolteon")]
 mod jolteon;
 #[cfg(feature = "jolteon")]
 pub use jolteon::*;
 
-#[cfg(feature = "pirateship")]
+#[cfg(feature = "cochin")]
 mod pirateship;
-#[cfg(feature = "pirateship")]
+#[cfg(feature = "cochin")]
 pub use pirateship::*;
