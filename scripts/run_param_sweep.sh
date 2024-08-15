73c559faffad9183b95615b5c2ca3676f50188f3
Shubham Mishra <grapheo12@gmail.com>
2024-08-15 12:26:03 +0000

    Big 7 node plots

--- scripts/run_param_sweep.sh
+++ scripts/run_param_sweep.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 set -o xtrace
 
-ALL_CLIENTS="-c 1 -c 10 -c 50 -c 100 -c 200 -c 500 -c 700 -c 1000"
+# ALL_CLIENTS="-c 1 -c 10 -c 50 -c 100 -c 200 -c 500 -c 700 -c 1000"
+ALL_CLIENTS="-c 10 -c 50 -c 100 -c 200 -c 500 -c 700 -c 1000"
 # # ALL_CLIENTS="-c 200 -c 500 -c 700 -c 1000"
 # ALL_CLIENTS="-c 50 -c 500"
 
 start_time=$(date -Ins)
 # start_time='2024-08-14T19:25:39.697429+00:00'
-RUN_CMD="python3 scripts/run_remote_client_sweep.py -nt scripts/local_template.json -ct scripts/local_client_template.json -ips ../nodelist.txt -i ../cluster_key.pem -r 3 -s 30 -up 2 -down 2 $ALL_CLIENTS"
+RUN_CMD="python3 scripts/run_remote_client_sweep.py -nt scripts/local_template.json -ct scripts/local_client_template.json -ips ../nodelist.txt -i ../cluster_key.pem -r 3 -s 120 -up 2 -down 2 $ALL_CLIENTS"
 # RUN_CMD="python3 scripts/run_remote_client_sweep.py -nt scripts/local_template.json -ct scripts/local_client_template.json -ips ../nodelist.txt -i ../cluster_key.pem -r 1 -s 30 -up 2 -down 2 $ALL_CLIENTS"
 
 # Run cochin quorum_diversity_k = 3, signature_max_delay_blocks = 500
@@ -63,12 +64,12 @@ end_time=$(date -Ins)
 # Plot together
 python3 scripts/plot_time_range_client_sweep.py \
     --path logs --end $end_time --start $start_time \
-    -r 3 -c 2 -l node1 -up 2 -down 2 -o plot.png \
-    --legend "cochin(k=3, sig=500)+byz" \
-    --legend "cochin(k=3, sig=50)+byz" \
-    --legend "cochin(k=3, sig=5)+byz" \
-    --legend "cochin(k=5, sig=5)+byz" \
-    --legend "cochin(k=1, sig=5)+byz" \
-    --legend "cochin(k=7, sig=5)+byz" \
+    -r 3 -c 2 -l node1 -up 5 -down 5 -o plot.png \
+    --legend "cochin(k=3, sig=500)" \
+    --legend "cochin(k=3, sig=50)" \
+    --legend "cochin(k=3, sig=5)" \
+    --legend "cochin(k=5, sig=5)" \
+    --legend "cochin(k=1, sig=5)" \
+    --legend "cochin(k=7, sig=5)" \
     --legend "pbft" \
     --legend "raft" \
