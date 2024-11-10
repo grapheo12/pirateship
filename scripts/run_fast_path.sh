7d54fd3da7784ec7218ae422c3edd88f06720692
Shubham Mishra <grapheo12@gmail.com>
2024-11-09 18:11:47 -0800

    C5 WAN setup

--- scripts/run_fast_path.sh
+++ scripts/run_fast_path.sh
@@ -11,20 +11,20 @@ set -o xtrace
 ALL_CLIENTS="-c 5000 -c 7000 -c 9000 -c 18000 -c 25000"
 # ALL_CLIENTS="-c 1000 -c 1200"
 
-# start_time=$(date -Ins)
-start_time='2024-10-30T20:35:31.196252+00:00'
+start_time=$(date -Ins)
+# start_time='2024-10-30T20:35:31.196252+00:00'
 RUN_CMD="python3 scripts/run_remote_client_sweep.py -nt /tmp/local_template.json -ct scripts/local_client_template.json -ips ../nodelist.txt -i ../cluster_key.pem -r 3 -s 120 -up 5 -down 5 $ALL_CLIENTS"
 # RUN_CMD="python3 scripts/run_remote_client_sweep.py -nt scripts/local_template.json -ct scripts/local_client_template.json -ips ../nodelist.txt -i ../cluster_key.pem -r 1 -s 30 -up 2 -down 2 $ALL_CLIENTS"
 
-# # Run pirateship quorum_diversity_k = 3, signature_max_delay_blocks = 50
-# jq '.consensus_config.quorum_diversity_k = 3 | .consensus_config.signature_max_delay_blocks = 50' scripts/local_template.json > /tmp/local_template.json
-# make
-# $RUN_CMD
+# Run pirateship quorum_diversity_k = 3, signature_max_delay_blocks = 50
+jq '.consensus_config.quorum_diversity_k = 3 | .consensus_config.signature_max_delay_blocks = 50' scripts/local_template.json > /tmp/local_template.json
+make
+$RUN_CMD
 
-# # Run pirateship quorum_diversity_k = 3, signature_max_delay_blocks = 50
-# jq '.consensus_config.quorum_diversity_k = 3 | .consensus_config.signature_max_delay_blocks = 50' scripts/local_template.json > /tmp/local_template.json
-# make pirateship_logger_nofast
-# $RUN_CMD
+# Run pirateship quorum_diversity_k = 3, signature_max_delay_blocks = 50
+jq '.consensus_config.quorum_diversity_k = 3 | .consensus_config.signature_max_delay_blocks = 50' scripts/local_template.json > /tmp/local_template.json
+make pirateship_logger_nofast
+$RUN_CMD
 
 
 end_time=$(date -Ins)
