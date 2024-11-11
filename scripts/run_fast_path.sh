cf6f5e5127ba250974183b2f3eb8e8bbb54027f6
Shubham Mishra <grapheo12@gmail.com>
2024-11-11 12:45:08 -0800

    C5 WAN setup: stable results

--- scripts/run_fast_path.sh
+++ scripts/run_fast_path.sh
@@ -8,8 +8,8 @@ set -o xtrace
 # ALL_CLIENTS="-c 1 -c 10 -c 50 -c 100 -c 200 -c 500 -c 700 -c 1000"
 # ALL_CLIENTS="-c 100 -c 200 -c 500 -c 700 -c 1000 -c 1200"
 # ALL_CLIENTS="-c 500 -c 700 -c 1000 -c 2000 -c 5000 -c 7000"
-# ALL_CLIENTS="-c 9000 -c 12000 -c 15000 -c 18000 -c 25000"
-ALL_CLIENTS="-c 15000 -c 18000 -c 25000"
+ALL_CLIENTS="-c 9000 -c 12000 -c 15000 -c 18000 -c 25000"
+# ALL_CLIENTS="-c 15000 -c 18000 -c 25000"
 # ALL_CLIENTS="-c 1000 -c 1200"
 
 start_time=$(date -Ins)
@@ -17,10 +17,10 @@ start_time=$(date -Ins)
 RUN_CMD="python3 scripts/run_remote_client_sweep.py -nt /tmp/local_template.json -ct scripts/local_client_template.json -ips ../nodelist.txt -i ../cluster_key.pem -r 3 -s 180 -up 5 -down 5 $ALL_CLIENTS"
 # RUN_CMD="python3 scripts/run_remote_client_sweep.py -nt scripts/local_template.json -ct scripts/local_client_template.json -ips ../nodelist.txt -i ../cluster_key.pem -r 1 -s 30 -up 2 -down 2 $ALL_CLIENTS"
 
-# # Run pirateship quorum_diversity_k = 3, signature_max_delay_blocks = 50
-# jq '.consensus_config.quorum_diversity_k = 3 | .consensus_config.signature_max_delay_blocks = 50' scripts/local_template.json > /tmp/local_template.json
-# make
-# $RUN_CMD
+# Run pirateship quorum_diversity_k = 3, signature_max_delay_blocks = 50
+jq '.consensus_config.quorum_diversity_k = 3 | .consensus_config.signature_max_delay_blocks = 50' scripts/local_template.json > /tmp/local_template.json
+make
+$RUN_CMD
 
 # Run pirateship quorum_diversity_k = 3, signature_max_delay_blocks = 50
 jq '.consensus_config.quorum_diversity_k = 3 | .consensus_config.signature_max_delay_blocks = 50' scripts/local_template.json > /tmp/local_template.json
@@ -34,6 +34,5 @@ end_time=$(date -Ins)
 python3 scripts/plot_time_range_client_sweep.py \
     --path logs --end $end_time --start $start_time \
     -r 3 -c 3 -l node1 -up 90 -down 5 -o plot.png \
+    --legend "pirateship_fast+byz" \
     --legend "pirateship_slow+byz"
-    # --legend "pirateship_fast+byz" \
-    # --legend "pirateship_slow+byz"
