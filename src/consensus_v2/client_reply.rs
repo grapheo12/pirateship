465bc7227cd64c0e64ae1f2dbff873231a2beeff
Shubham Mishra <grapheo12@gmail.com>
2025-02-12 23:34:12 +0000

    Parallel reply processing

--- src/consensus_v2/client_reply.rs
+++ src/consensus_v2/client_reply.rs
@@ -2,9 +2,9 @@ use std::{collections::HashMap, sync::Arc};
 
 use log::error;
 use prost::Message as _;
-use tokio::sync::{oneshot, Mutex};
+use tokio::{sync::{oneshot, Mutex}, task::JoinSet};
 
-use crate::{config::AtomicConfig, crypto::HashType, proto::{client::{ProtoClientReply, ProtoTransactionReceipt}, execution::ProtoTransactionResult, rpc::ProtoPayload}, rpc::{server::LatencyProfile, PinnedMessage}, utils::channel::Receiver};
+use crate::{config::AtomicConfig, crypto::HashType, proto::{client::{ProtoClientReply, ProtoTransactionReceipt}, execution::ProtoTransactionResult, rpc::ProtoPayload}, rpc::{server::LatencyProfile, PinnedMessage}, utils::channel::{Receiver, Sender}};
 
 use super::batch_proposal::MsgAckChanWithTag;
 
@@ -12,6 +12,11 @@ pub enum ClientReplyCommand {
     CancelAllRequests,
     CrashCommitAck(HashMap<HashType, (u64, Vec<ProtoTransactionResult>)>),
     ByzCommitAck(HashMap<HashType, (u64, Vec<ProtoTransactionResult>)>),
+}
+
+enum ReplyProcessorCommand {
+    CrashCommit(u64 /* block_n */, u64 /* tx_n */, HashType, ProtoTransactionResult /* result */, MsgAckChanWithTag),
+    ByzCommit(u64 /* block_n */, u64 /* tx_n */, ProtoTransactionResult /* result */, MsgAckChanWithTag),
 }
 pub struct ClientReplyHandler {
     config: AtomicConfig,
@@ -22,7 +27,10 @@ pub struct ClientReplyHandler {
     reply_map: HashMap<HashType, Vec<MsgAckChanWithTag>>,
 
     crash_commit_reply_buf: HashMap<HashType, (u64, Vec<ProtoTransactionResult>)>,
-    byz_commit_reply_buf: HashMap<HashType, (u64, Vec<ProtoTransactionResult>)>
+    byz_commit_reply_buf: HashMap<HashType, (u64, Vec<ProtoTransactionResult>)>,
+
+    reply_processors: JoinSet<()>,
+    reply_processor_queue: (flume::Sender<ReplyProcessorCommand>, flume::Receiver<ReplyProcessorCommand>),
 }
 
 impl ClientReplyHandler {
@@ -31,6 +39,7 @@ impl ClientReplyHandler {
         batch_rx: Receiver<(oneshot::Receiver<HashType>, Vec<MsgAckChanWithTag>)>,
         reply_command_rx: Receiver<ClientReplyCommand>,
     ) -> Self {
+        let _chan_depth = config.get().rpc_config.channel_depth as usize;
         Self {
             config,
             batch_rx,
@@ -38,11 +47,49 @@ impl ClientReplyHandler {
             reply_map: HashMap::new(),
             crash_commit_reply_buf: HashMap::new(),
             byz_commit_reply_buf: HashMap::new(),
+            reply_processors: JoinSet::new(),
+            reply_processor_queue: flume::bounded(_chan_depth),
         }
     }
 
     pub async fn run(client_reply_handler: Arc<Mutex<Self>>) {
         let mut client_reply_handler = client_reply_handler.lock().await;
+        for _ in 0..10 {
+            let rx = client_reply_handler.reply_processor_queue.1.clone();
+            client_reply_handler.reply_processors.spawn(async move {
+                while let Ok(cmd) = rx.recv_async().await {
+                    match cmd {
+                        ReplyProcessorCommand::CrashCommit(block_n, tx_n, hsh, reply, (reply_chan, client_tag)) => {
+                            let reply = ProtoClientReply {
+                                reply: Some(
+                                    crate::proto::client::proto_client_reply::Reply::Receipt(
+                                        ProtoTransactionReceipt {
+                                            req_digest: hsh,
+                                            block_n,
+                                            tx_n,
+                                            results: Some(reply),
+                                            await_byz_response: false,
+                                            byz_responses: vec![],
+                                        },
+                                )),
+                                client_tag
+                            };
+                
+                            let reply_ser = reply.encode_to_vec();
+                            let _sz = reply_ser.len();
+                            let reply_msg = PinnedMessage::from(reply_ser, _sz, crate::rpc::SenderType::Anon);
+                            let latency_profile = LatencyProfile::new();
+                            
+                            let _ = reply_chan.send((reply_msg, latency_profile)).await;
+                        },
+                        ReplyProcessorCommand::ByzCommit(_, _, result, sender) => {
+
+                        },
+                    }
+                }
+            });
+        }
+        
         loop {
             if let Err(_) = client_reply_handler.worker().await {
                 break;
@@ -80,27 +127,7 @@ impl ClientReplyHandler {
     async fn do_crash_commit_reply(&mut self, reply_sender_vec: Vec<MsgAckChanWithTag>, hash: HashType, n: u64, reply_vec: Vec<ProtoTransactionResult>) {
         assert_eq!(reply_sender_vec.len(), reply_vec.len());
         for (tx_n, ((reply_chan, client_tag), reply)) in reply_sender_vec.into_iter().zip(reply_vec.into_iter()).enumerate() {
-            let reply = ProtoClientReply {
-                reply: Some(
-                    crate::proto::client::proto_client_reply::Reply::Receipt(
-                        ProtoTransactionReceipt {
-                            req_digest: hash.clone(),
-                            block_n: n,
-                            tx_n: tx_n as u64,
-                            results: Some(reply),
-                            await_byz_response: false,
-                            byz_responses: vec![],
-                        },
-                )),
-                client_tag
-            };
-
-            let reply_ser = reply.encode_to_vec();
-            let _sz = reply_ser.len();
-            let reply_msg = PinnedMessage::from(reply_ser, _sz, crate::rpc::SenderType::Anon);
-            let latency_profile = LatencyProfile::new();
-            
-            let _ = reply_chan.send((reply_msg, latency_profile)).await;
+            self.reply_processor_queue.0.send_async(ReplyProcessorCommand::CrashCommit(n, tx_n as u64, hash.clone(), reply, (reply_chan, client_tag))).await.unwrap();
         }
     }
 
