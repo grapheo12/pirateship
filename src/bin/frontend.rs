739aa2abcee2544adaf486f4e3241329a8d38772
chawinphat <chawinphat@berkeley.edu>
2025-02-26 21:01:27 -0800

    starting to add front end

---
+++ src/bin/frontend.rs
@@ -0,0 +1,50 @@
+use actix_web::{put, get, web, App, HttpResponse, HttpServer, Responder};
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+
+type Store = Arc<Mutex<HashMap<String, String>>>;
+
+#[put("/set/{key}")]
+async fn set_key(store: web::Data<Store>, key: web::Path<String>, value: web::Json<String>) -> impl Responder {
+    let key_str = key.into_inner();
+    let value_str = value.into_inner();
+
+    HttpResponse::Ok().json(serde_json::json!({
+        "message": "Key set successfully",
+        "key": key_str,
+        "value": value_str
+    }))
+}
+
+#[get("/get/{key}")]
+async fn get_key(store: web::Data<Store>, key: web::Path<String>) -> impl Responder {
+    let key_str = key.into_inner();
+    HttpResponse::Ok().json(serde_json::json!({
+        "message": "Key found successfully",
+        "key": key_str,
+    }))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let store: Store = Arc::new(Mutex::new(HashMap::new()));
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(store.clone()))
+            .service(set_key)
+            .service(get_key)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
+
+//tests
+/*
+curl -X GET "http://localhost:8080/get/username"
+
+curl -X PUT "http://localhost:8080/set/username" -H "Content-Type: application/json" -d '"john_doe"'
+
+*/
+
