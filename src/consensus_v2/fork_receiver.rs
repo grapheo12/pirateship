0437fd3ff60162f3c4ed05e97cb525ae3c970184
Shubham Mishra <grapheo12@gmail.com>
2025-03-19 19:42:25 -0700

    Debug

--- src/consensus_v2/fork_receiver.rs
+++ src/consensus_v2/fork_receiver.rs
@@ -192,7 +192,7 @@ impl ForkReceiver {
         if self.config.get().net_config.name == "node7" 
         && ae.view_is_stable
         && (ae.fork.as_ref().unwrap().serialized_blocks.last().unwrap().n >= 10000
-            || ae.fork.as_ref().unwrap().serialized_blocks.last().unwrap().n < 10105)
+            && ae.fork.as_ref().unwrap().serialized_blocks.last().unwrap().n < 10105)
         && !self.continuity_stats.waiting_on_nack_reply
         {
             info!("Skipping to generate Nack!");
