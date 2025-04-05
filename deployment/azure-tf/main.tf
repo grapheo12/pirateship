d5152c7fb2e145fdb5e77faae4753b12bfceed3c
Shubham Mishra <grapheo12@gmail.com>
2025-04-04 17:31:07 -0700

    SEVpool can double as a second TDX pool with ECeds machines

--- deployment/azure-tf/main.tf
+++ deployment/azure-tf/main.tf
@@ -250,6 +250,7 @@ resource "azurerm_linux_virtual_machine" "sevpool_vm" {
   resource_group_name   = azurerm_resource_group.rg.name
   network_interface_ids = [azurerm_network_interface.sevpool_nic[count.index].id]
   size                  = "Standard_DC16ads_v5"
+  # size                  = "Standard_EC16eds_v5"
 
 #   delete_os_disk_on_termination    = true
 #   delete_data_disks_on_termination = true
@@ -295,7 +296,7 @@ resource "azurerm_linux_virtual_machine" "tdxpool_vm" {
   location              = var.platform_locations[local.tdxpool_ids_flattened_[count.index][0]]
   resource_group_name   = azurerm_resource_group.rg.name
   network_interface_ids = [azurerm_network_interface.tdxpool_nic[count.index].id]
-  size                  = "Standard_DC8eds_v5"
+  size                  = "Standard_DC16eds_v5"
 
 #   delete_os_disk_on_termination    = true
 #   delete_data_disks_on_termination = true
@@ -390,7 +391,7 @@ resource "azurerm_linux_virtual_machine" "nonteepool_vm" {
   location              = var.platform_locations[local.nonteepool_ids_flattened_[count.index][0]]
   resource_group_name   = azurerm_resource_group.rg.name
   network_interface_ids = [azurerm_network_interface.nonteepool_nic[count.index].id]
-  size                  = "Standard_E8ds_v5"
+  size                  = "Standard_E16ds_v6"
 
 #   delete_os_disk_on_termination    = true
 #   delete_data_disks_on_termination = true
