b58f2e8cd1f02568955b375e8656a21c6ba174c2
Natacha Crooks <v-ncrooks@microsoft.com>
2025-04-07 06:17:16 -0700

    Merge conflict fixes

--- src/client/workload_generators/mod.rs
+++ src/client/workload_generators/mod.rs
@@ -1,10 +1,6 @@
 use crate::proto::execution::{ProtoTransaction, ProtoTransactionResult};
 
-<<<<<<< HEAD
-#[derive(Clone)]
-=======
 #[derive(Clone, Copy, Debug, Eq, PartialEq, PartialOrd, Ord)]
->>>>>>> origin/crazy_rewrite
 pub enum Executor {
     Leader = 1,
     Any = 2
