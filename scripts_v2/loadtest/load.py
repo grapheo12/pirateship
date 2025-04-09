469e6666f9711d0c195a7996b9c66ef619e8498e
Shubham Mishra <grapheo12@gmail.com>
2025-04-09 12:44:19 -0700

    KMS debug

--- scripts_v2/loadtest/load.py
+++ scripts_v2/loadtest/load.py
@@ -13,20 +13,19 @@ MAX_CONCURRENT_REQUESTS = 200
 
 RAND_SEED_LIST = [42, 120, 430, 82, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110]
 
-async def register_user(host, usernames, password, connector):
-    async with aiohttp.ClientSession(connector=connector) as session:
-        try: 
-            for username in usernames:
-                async with session.post(f"{host}/register", json={"username": username, "password": password}) as response:
-                    if response.status != 200:
-                        print(f"Error registering {username}: {await response.text()}")
+async def register_user(host, usernames, password, session):
+    try: 
+        for username in usernames:
+            async with session.post(f"{host}/register", json={"username": username, "password": password}) as response:
+                if response.status != 200:
+                    print(f"Error registering {username}: {await response.text()}")
 
-                async with session.post(f"{host}/refresh", json={"username": username, "password": password}) as response:
-                    if response.status != 200:
-                        print(f"Error refreshing {username}: {await response.text()}")
-        except Exception as e:
-            await session.close()
-            print(f"An error occurred: {e}")
+            async with session.post(f"{host}/refresh", json={"username": username, "password": password}) as response:
+                if response.status != 200:
+                    print(f"Error refreshing {username}: {await response.text()}")
+    except Exception as e:
+        await session.close()
+        print(f"An error occurred: {e}")
 
 async def create_secret(host, usernames, password, session, nodes, threshold):
     try:
